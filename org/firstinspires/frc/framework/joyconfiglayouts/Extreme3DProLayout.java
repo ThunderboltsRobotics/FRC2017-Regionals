ed8f46f01b07bab6673de9e06617248c159da30e
YoshiRulz <yoshiisawsm@gmail.com>
2016-03-19 18:09:32 +1100

    Layout for Logitech's Extreme 3D Pro controller

---
+++ org/firstinspires/frc/framework/joyconfiglayouts/Extreme3DProLayout.java
@@ -0,0 +1,28 @@
+package org.firstinspires.frc4739.config.joyconfiglayouts;
+
+public class Extreme3DProLayout {
+	public static final int FORWARDS = 1;
+	public static final int REVERSE = -1;
+
+	public static final int AXIS_THROTTLE = 3;
+	public static final int AXIS_THROTTLE_DIRECTION = REVERSE;
+	public static final int AXIS_X = 0;
+	public static final int AXIS_X_DIRECTION = FORWARDS;
+	public static final int AXIS_Y = 1;
+	public static final int AXIS_Y_DIRECTION = REVERSE;
+	public static final int AXIS_Z = 2;
+	public static final int AXIS_Z_DIRECTION = REVERSE;
+
+	public static final int BUTTON_TRIGGER = 1;
+	public static final int BUTTON_2 = 2;
+	public static final int BUTTON_3 = 3;
+	public static final int BUTTON_4 = 4;
+	public static final int BUTTON_5 = 5;
+	public static final int BUTTON_6 = 6;
+	public static final int BUTTON_7 = 7;
+	public static final int BUTTON_8 = 8;
+	public static final int BUTTON_9 = 9;
+	public static final int BUTTON_10 = 10;
+	public static final int BUTTON_11 = 11;
+	public static final int BUTTON_12 = 12;
+}
