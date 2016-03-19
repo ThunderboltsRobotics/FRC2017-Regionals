000b4926c48160d31da42eb933e7dd4a3caab17f
YoshiRulz <yoshiisawsm@gmail.com>
2016-03-19 18:09:09 +1100

    Standard gaming controllers

---
+++ org/firstinspires/frc/framework/joyconfiglayouts/PS3Layout.java
@@ -0,0 +1,31 @@
+package org.firstinspires.frc4739.config.joyconfiglayouts;
+
+public class PS3Layout {
+	public static final int FORWARDS = 1;
+	public static final int REVERSE = -1;
+
+	public static final int AXIS_LS_X = 0;
+	public static final int AXIS_LS_X_DIRECTION = FORWARDS;
+	public static final int AXIS_LS_Y = 1;
+	public static final int AXIS_LS_Y_DIRECTION = REVERSE;
+	public static final int AXIS_L2 = 2;
+	public static final int AXIS_R2 = 3;
+	public static final int AXIS_RS_X = 4;
+	public static final int AXIS_RS_X_DIRECTION = FORWARDS;
+	public static final int AXIS_RS_Y = 5;
+	public static final int AXIS_RS_Y_DIRECTION = REVERSE;
+
+	public static final int BUTTON_CROSS = 1;
+	public static final int BUTTON_X = BUTTON_CROSS;
+	public static final int BUTTON_CIRCLE = 2;
+	public static final int BUTTON_O = BUTTON_CIRCLE;
+	public static final int BUTTON_SQUARE = 3;
+	public static final int BUTTON_TRIANGLE = 4;
+	public static final int BUTTON_L1 = 5;
+	public static final int BUTTON_R2 = 6;
+	public static final int BUTTON_SELECT = 7;
+	public static final int BUTTON_START = 8;
+	public static final int BUTTON_L3 = 9;
+	public static final int BUTTON_R3 = 10;
+	public static final int BUTTON_PS = 11;
+}
---
+++ org/firstinspires/frc/framework/joyconfiglayouts/XBox360Layout.java
@@ -0,0 +1,28 @@
+package org.firstinspires.frc4739.config.joyconfiglayouts;
+
+public class XBox360Layout {
+	public static final int FORWARDS = 1;
+	public static final int REVERSE = -1;
+
+	public static final int AXIS_LS_X = 0;
+	public static final int AXIS_LS_X_DIRECTION = FORWARDS;
+	public static final int AXIS_LS_Y = 1;
+	public static final int AXIS_LS_Y_DIRECTION = REVERSE;
+	public static final int AXIS_LT = 2;
+	public static final int AXIS_RT = 3;
+	public static final int AXIS_RS_X = 4;
+	public static final int AXIS_RS_X_DIRECTION = FORWARDS;
+	public static final int AXIS_RS_Y = 5;
+	public static final int AXIS_RS_Y_DIRECTION = REVERSE;
+
+	public static final int BUTTON_A = 1;
+	public static final int BUTTON_B = 2;
+	public static final int BUTTON_X = 3;
+	public static final int BUTTON_Y = 4;
+	public static final int BUTTON_LB = 5;
+	public static final int BUTTON_RB = 6;
+	public static final int BUTTON_BACK = 7;
+	public static final int BUTTON_START = 8;
+	public static final int BUTTON_LS = 9;
+	public static final int BUTTON_RS = 10;
+}
