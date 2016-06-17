de1855cd1c60b83c97470b33502e20b50fb44685
James Groom <yoshiisawsm@gmail.com>
2016-06-17 13:34:40 +1000

    add vision

--- org/firstinspires/frc/_4739/RobotMain.java
+++ org/firstinspires/frc/_4739/RobotMain.java
@@ -1,5 +1,7 @@
 package org.firstinspires.frc._4739;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import org.firstinspires.frc.framework.abstraction.BetterRobot;
 import org.firstinspires.frc.framework.abstraction.MatchPhaseRoutine;
 
@@ -11,6 +13,7 @@ public class RobotMain extends BetterRobot {
 	private MatchPhaseRoutine disabled = new Disabled();
 	private MatchPhaseRoutine auto = new Auto();
 	private MatchPhaseRoutine teleop = new Teleop();
+	private MatchPhaseRoutine vision_teleop = new VisionTeleop();
 //	private MatchPhaseRoutine test = new MyTestRoutine();
 
 	public void robotInit() { Hardware.init(); }
@@ -19,7 +22,13 @@ public class RobotMain extends BetterRobot {
 	public void disabledInit() { disabled.start(); }
 	public void disabledPeriodic() { disabled.tick(); }
 	public void teleopInit() { teleop.start(); }
-	public void teleopPeriodic() { teleop.tick(); }
+	public void teleopPeriodic() {
+		if (SmartDashboard.getBoolean("vision")) {
+			visionteleop.tick();
+		} else {
+			teleop.tick();
+		}
+	}
 //	public void testInit() { test.start(); }
 //	public void testPeriodic() { test.tick(); }
 }
