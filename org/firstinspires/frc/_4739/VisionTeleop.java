ab5393908242b3a53879b69413b9ec0ab07a0e01
James Groom <yoshiisawsm@gmail.com>
2016-06-20 09:54:57 +1000

    almost done vision

--- org/firstinspires/frc/_4739/VisionTeleop.java
+++ org/firstinspires/frc/_4739/VisionTeleop.java
@@ -1,14 +1,19 @@
 package org.firstinspires.frc._4739;
 
 import org.firstinspires.frc.framework.abstraction.MatchPhaseRoutine;
-import org.firstinspires.frc._4739.JoyConfig;
 
 /**
  * 4739's vision tracking routine for the 2016 game (FIRST Stronghold).
  * @author FRC 4739 Thunderbolts Robotics
  */
 public class Teleop extends MatchPhaseRoutine {
-	private static final double DRIVE_SPEED_MULTIPLIER = 0.3;
+	private final double centerX = 1 / 2;
+	private final double centerY = 2 / 3;
+	private final double hitZoneX = 0.01;
+	private final double hitZoneY = 0.01;
+	private final double maxSpeedX = 0.3;
+	private final double maxSpeedY = 1;
+	private GRIPResult;
 
 	public void start() {
 		//Kill auto
@@ -17,24 +22,21 @@ public class Teleop extends MatchPhaseRoutine {
 
 	public void tick() {
 		//TODO get GRIP telemetry
-		private final double centerX = 1 / 2;
-		private final double centerY = 1 / 2;
+		//GRIPResult = ;
 		private final double goalX = GRIPResult.x + (GRIPResult.w / 2);
 		private final double goalY = GRIPResult.y + (GRIPResult.h / 2);
-		private final double hitZoneX = 0.01;
-		private final double hitZoneY = 0.01;
 
 		if (goalX < centerX - hitZoneX) {
-			Hardware.TankDrive.right( * DRIVE_SPEED_MULTIPLIER);
+			Hardware.TankDrive.right(Math.sqrt(centerX - goalX) * maxSpeedX);
 		} else if (goalX > centerX + hitZoneY) {
-			Hardware.TankDrive.left(rightTankPower * DRIVE_SPEED_MULTIPLIER);
+			Hardware.TankDrive.left(Math.sqrt(goalX - centerX) * maxSpeedX);
 		} else {
 			Hardware.TankDrive.stop();
 		}
 		if (goalY < centerY - hitZoneY) {
-			Hardware.FrontArm.drive(leftTankPower * DRIVE_SPEED_MULTIPLIER);
+			Hardware.FrontArm.drive(Math.sqrt(goalY - centerY) * maxSpeedY);
 		} else if (goalY > centerY + hitZoneY) {
-			Hardware.FrontArm.drive(rightTankPower * DRIVE_SPEED_MULTIPLIER);
+			Hardware.FrontArm.drive(Math.sqrt(centerY - goalY) * maxSpeedY);
 		} else {
 			Hardware.FrontArm.drive(0);
 		}
