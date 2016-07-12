ed52797b691b77250880bca5e80f753f44e00662
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-12 13:10:14 +1000

    Moved

--- org/firstinspires/frc/framework/abstraction/MatchPhaseRoutine.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.frc.framework.abstraction;
-
-public abstract class MatchPhaseRoutine {
-	public abstract void start();
-	public abstract void tick();
-}
---
+++ org/firstinspires/frc/framework/software/MatchPhaseRoutine.java
@@ -0,0 +1,6 @@
+package org.firstinspires.frc.framework.software;
+
+public abstract class MatchPhaseRoutine {
+	public abstract void start();
+	public abstract void loop();
+}
