8bd7af28da58e52ae69dc4c245289f94283b832e
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-14 12:39:15 +1000

    Updated to latest framework, vision now uses the controller

--- org/firstinspires/frc/_4739/RobotMain.java
+++ org/firstinspires/frc/_4739/RobotMain.java
@@ -1,34 +1,53 @@
 package org.firstinspires.frc._4739;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import org.firstinspires.frc.framework.abstraction.BetterRobot;
-import org.firstinspires.frc.framework.abstraction.MatchPhaseRoutine;
+import org.firstinspires.frc._4739.phases.*;
+import org.firstinspires.frc.framework.granulation.BetterRobot;
+import org.firstinspires.frc.framework.software.MatchPhaseRoutine;
+import org.firstinspires.frc.framework.software.hid_controller_layouts.XBOX360Controller;
 
 /**
  * Main class
  * @author FRC 4739 Thunderbolts Robotics
+ * @version 2016-07-10/00
  */
 public class RobotMain extends BetterRobot {
-	private MatchPhaseRoutine disabled = new Disabled();
-	private MatchPhaseRoutine auto = new Auto();
-	private MatchPhaseRoutine teleop = new Teleop();
-	private MatchPhaseRoutine vision_teleop = new VisionTeleop();
-//	private MatchPhaseRoutine test = new MyTestRoutine();
+	private final MatchPhaseRoutine disabled = new Disabled();
+	private final MatchPhaseRoutine auto = new Auto();
+	private final MatchPhaseRoutine teleop = new Teleop();
+	private final MatchPhaseRoutine vision_teleop = new VisionTeleop();
+	private final MatchPhaseRoutine test = new TestTeleop();
 
-	public void robotInit() { Hardware.init(); }
-	public void autonomousInit() { auto.start(); }
-	public void autonomousPeriodic() { auto.tick(); }
-	public void disabledInit() { disabled.start(); }
-	public void disabledPeriodic() { disabled.tick(); }
-	public void teleopInit() { teleop.start(); }
-	public void teleopPeriodic() {
-		if (SmartDashboard.getBoolean("vision")) {
-			visionteleop.tick();
+	public void initialize() {
+		HW.initialize();
+		disabled.start();
+	}
+	public void autoStart() {
+		auto.start();
+	}
+	public void autoLoop() {
+		auto.loop();
+	}
+	public void disabledStart() {
+		disabled.start();
+	}
+	public void disabledLoop() {
+		disabled.loop();
+	}
+	public void teleopStart() {
+		teleop.start();
+		vision_teleop.start();
+	}
+	public void teleopLoop() {
+		if (HW.gamepad1.getButton(XBOX360Controller.Buttons.A)) {
+			vision_teleop.loop();
 		} else {
-			teleop.tick();
+			teleop.loop();
 		}
 	}
-//	public void testInit() { test.start(); }
-//	public void testPeriodic() { test.tick(); }
+	public void testStart() {
+		test.start();
+	}
+	public void testLoop() {
+		test.loop();
+	}
 }
