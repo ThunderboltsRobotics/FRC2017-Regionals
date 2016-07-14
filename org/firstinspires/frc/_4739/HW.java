123f8e35876d5c297d3a646996c883c1a85dbb00
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-14 12:35:18 +1000

    Shorthand for Hardware

---
+++ org/firstinspires/frc/_4739/HW.java
@@ -0,0 +1,11 @@
+package org.firstinspires.frc._4739;
+
+/**
+ * Shorthand for Hardware in this package.
+ * @author FRC 4739 Thunderbolts Robotics
+ * @see Hardware
+ * @version 2016-07-11/00
+ */
+public class HW extends Hardware {
+
+}
