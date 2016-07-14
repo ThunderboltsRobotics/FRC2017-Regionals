5775e49de683170e6544d243afb82c565b71f967
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-14 12:40:07 +1000

    Updated to latest framework, replaced front arm with shooter, changed heirarchy

--- org/firstinspires/frc/_4739/Hardware.java
+++ org/firstinspires/frc/_4739/Hardware.java
@@ -1,42 +1,133 @@
 package org.firstinspires.frc._4739;
 
-import org.firstinspires.frc._4739.RioPortConfig;
-
-import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.Victor;
+import org.firstinspires.frc.framework.abstraction.DSGamepadIndex;
+import org.firstinspires.frc.framework.abstraction.RioCANID;
+import org.firstinspires.frc.framework.abstraction.RioHWPort;
+import org.firstinspires.frc.framework.hardware.MotorController;
+import org.firstinspires.frc.framework.hardware.MotorControllerType;
+//import org.firstinspires.frc.framework.hardware.MotorEncoder;
+//import org.firstinspires.frc.framework.hardware.PWMLEDArray;
+import org.firstinspires.frc.framework.software.hid_controller_layouts.LogitechF310Gamepad;
 
 /**
  * 4739's hardware setup for the 2016 game (FIRST Stronghold).
  * @author FRC 4739 Thunderbolts Robotics
+ * @version 2016-07-10/11
  */
 public class Hardware {
-	private static final double RIGHT_SPEED_MULTIPLIER = 0.95; //Our Talons aren't calibrated or something
-
-	public static final Talon[] talons = new Talon[10];
-	public static final Victor[] victors = new Victor[10];
-
-	public static final void init() {
-		talons[RioPortConfig.leftDriveA] = new Talon(RioPortConfig.leftDriveA);
-		talons[RioPortConfig.leftDriveB] = new Talon(RioPortConfig.leftDriveB);
-		talons[RioPortConfig.rightDriveA] = new Talon(RioPortConfig.rightDriveA);
-		talons[RioPortConfig.rightDriveB] = new Talon(RioPortConfig.rightDriveB);
-		victors[RioPortConfig.frontArm] = new Victor(RioPortConfig.frontArm);
+	private static class PWMPorts {
+		//TODO switch HW - 0,1 w/ 2,3
+		static final RioHWPort driveLeftA = RioHWPort.PWM_0;
+		static final RioHWPort driveLeftB = RioHWPort.PWM_1;
+		static final RioHWPort driveRightA = RioHWPort.PWM_2;
+		static final RioHWPort driveRightB = RioHWPort.PWM_3;
+		static final RioHWPort shooterBack = RioHWPort.PWM_4;
+		static final RioHWPort shooterLeft = RioHWPort.PWM_5;
+		static final RioHWPort shooterRight = RioHWPort.PWM_6;
+//		static final RioHWPort LEDRing = RioHWPort.PWM_9;
+	}
+	private static class CANIDs {
+		static final RioCANID shooterAim = new RioCANID(0);
 	}
 
-	public static final class TankDrive {
-		public static final void left(double d) {
-			talons[RioPortConfig.leftDriveA].set(d * RIGHT_SPEED_MULTIPLIER);
-			talons[RioPortConfig.leftDriveB].set(d * RIGHT_SPEED_MULTIPLIER);
+	public static class Motors {
+		public static class Drive {
+			private static final MotorController leftA = new MotorController(PWMPorts.driveLeftA, MotorControllerType.TalonSR);
+			private static final MotorController leftB = new MotorController(PWMPorts.driveLeftB, MotorControllerType.TalonSR);
+			private static final MotorController rightA = new MotorController(PWMPorts.driveRightA, MotorControllerType.TalonSR);
+			private static final MotorController rightB = new MotorController(PWMPorts.driveRightB, MotorControllerType.TalonSR);
+			public static void left(double d) {
+				leftA.setSpeed(d);
+				leftB.setSpeed(d);
+			}
+			public static void right(double d) {
+				rightA.setSpeed(d);
+				rightB.setSpeed(d);
+			}
+			@SuppressWarnings("SameParameterValue")
+			public static void drive(double l, double r) {
+				left(l);
+				right(r);
+			}
+			public static void stop() {
+				left(0);
+				right(0);
+			}
 		}
-		public static final void right(double d) {
-			talons[RioPortConfig.rightDriveA].set(-d);
-			talons[RioPortConfig.rightDriveB].set(-d);
+		public static class Shooter {
+			private static final double FIRE_SPEED = 1;
+			private static final double INTAKE_SPEED = -0.3;
+			private static final double KICKER_SPEED = 1;
+			private static final double AIM_MAX_SPEED = 1;
+
+			private static final MotorController aim;
+			static {
+				MotorController temp;
+				try {
+					temp = new MotorController(CANIDs.shooterAim, MotorControllerType.TalonSRX);
+				} catch (MotorController.CANIsUnsupportedException e) {
+					e.printStackTrace();
+					temp = null;
+					//Graceful shutdown
+					System.exit(0);
+				}
+				aim = temp;
+			}
+
+			private static final MotorController back = new MotorController(PWMPorts.shooterBack, MotorControllerType.Victor);
+			private static final MotorController left = new MotorController(PWMPorts.shooterLeft, MotorControllerType.Victor);
+			private static final MotorController right = new MotorController(PWMPorts.shooterRight, MotorControllerType.Victor);
+//			private static final MotorEncoder aimEncoder = new MotorEncoder(0);
+
+			public static void aim(double d) {
+				relAim(Math.sqrt(/*aimEncoder.getPos()*/ - d) * AIM_MAX_SPEED);
+			}
+			public static void relAim(double d) {
+				Shooter.aim.setSpeed(d);
+			}
+			private static void run(double d) {
+				//TODO check direction
+				Shooter.left.setSpeed(d);
+				Shooter.right.setSpeed(d);
+				Shooter.back.setSpeed(KICKER_SPEED);
+			}
+			public static void fire() {
+				run(FIRE_SPEED);
+			}
+			public static void intake() {
+				run(INTAKE_SPEED);
+			}
+			public static void stop() {
+				run(0);
+			}
+		}
+		/*public static class LEDRing {
+			//private static final PWMLEDArray noIdeaWhatThisDoes = new PWMLEDArray(PWMPorts.LEDRing);
+			//private static SPI noIdeaWhatThisDoes;
+			public static void off() {
+				noIdeaWhatThisDoes.set(0);
+			}
+			public static void on() {
+				noIdeaWhatThisDoes.set(1);
+			}
+			//white - signal
+			//red - power (+)
+			//black - power (-)
+		}*/
+	}
+
+	static void initialize() {
+		if (!Motors.Drive.rightA.isReversed()) {
+			Motors.Drive.rightA.reverse();
+		}
+		if (!Motors.Drive.rightB.isReversed()) {
+			Motors.Drive.rightB.reverse();
+		}
+		if (!Motors.Shooter.right.isReversed()) {
+			Motors.Shooter.right.reverse();
 		}
 	}
 
-	public static final class FrontArm {
-		public static final void drive(double d) {
-			victors[RioPortConfig.frontArm].set(d);
-		}
-	}
+	public static final LogitechF310Gamepad gamepad1 = new LogitechF310Gamepad(new DSGamepadIndex(0));
+	public static final LogitechF310Gamepad gamepad2 = new LogitechF310Gamepad(new DSGamepadIndex(1));
 }
