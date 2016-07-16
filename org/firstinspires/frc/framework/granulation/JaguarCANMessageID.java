7024c12754f4c2abf2092f7ff762f14403f2e9b9
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-16 20:51:03 +1000

    Shuffled code around, merged some methods, added VerificationMask to CANMessages

--- org/firstinspires/frc/framework/granulation/JaguarCANMessageID.java
+++ org/firstinspires/frc/framework/granulation/JaguarCANMessageID.java
@@ -2,10 +2,11 @@ package org.firstinspires.frc.framework.granulation;
 
 /**
  * @author FRC 4739 Thunderbolts Robotics
- * @version 2016-07-16/00
+ * @version 2016-07-16/02
  */
 enum JaguarCANMessageID {
 	NULL(0),
+	VerificationMask(536870911),
 	set_Percent(33685824), set_Current(33687040), set_Speed(33689088), set_Position(33690048), set_Voltage(33687936),
 	setP_Current(33686720), setP_Speed(33688768), setP_Position(33689792),
 	setI_Current(33686784), setI_Speed(33688832), setI_Position(33689856),
