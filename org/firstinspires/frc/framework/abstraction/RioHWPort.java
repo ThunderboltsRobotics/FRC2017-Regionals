d693ccbfd5f77c05548836a4d6ddcece7db35f9f
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-20 20:06:48 +1000

    Fixed docs

--- org/firstinspires/frc/framework/abstraction/RioHWPort.java
+++ org/firstinspires/frc/framework/abstraction/RioHWPort.java
@@ -3,10 +3,9 @@ package org.firstinspires.frc.framework.abstraction;
 import java.util.InputMismatchException;
 
 /**
- * NOTE: THIS CLASS IS NOT FOR USE!
- * This was intended to replace the existing PWM port enum, and allow for other ports e.g. analog, SPI. Unfortunately, the implementation of motor controllers currently requires extending WPILibJ's SafePWM class, and an initial call to super in the motor controller's builder prevents the correct port type testing.
+ * Represents a physical port on the RoboRIO.
  * @author FRC 4739 Thunderbolts Robotics
- * @version 2016-07-10/01
+ * @version 2016-07-20/00
  */
 public enum RioHWPort {
 	PWM_0(PortType.PWM, 0), PWM_1(PortType.PWM, 1), PWM_2(PortType.PWM, 2), PWM_3(PortType.PWM, 3), PWM_4(PortType.PWM, 4), PWM_5(PortType.PWM, 5), PWM_6(PortType.PWM, 6), PWM_7(PortType.PWM, 7), PWM_8(PortType.PWM, 8), PWM_9(PortType.PWM, 9);
