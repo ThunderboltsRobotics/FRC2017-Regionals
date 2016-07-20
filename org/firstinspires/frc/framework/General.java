453e4eb36c4327c1a3aaca1702fb520996a42cd7
YoshiRulz <yoshiisawsm@gmail.com>
2016-07-20 20:06:26 +1000

    Shuffled code and added plurality to ExtraParametersNotApplicableException text

--- org/firstinspires/frc/framework/General.java
+++ org/firstinspires/frc/framework/General.java
@@ -1,16 +1,11 @@
 package org.firstinspires.frc.framework;
 
 /**
+ * Things that could be used anywhere.
  * @author FRC 4739 Thunderbolts Robotics
  * @version 2016-07-19/00
  */
 public class General {
-	public static final class ExtraParametersNotApplicableException extends IllegalArgumentException {
-		public ExtraParametersNotApplicableException(String methodName, String[] parameters) {
-			super("The extra argument for " + methodName + "(..." + implodeStringArray(parameters) + ") isn't used there");
-		}
-	}
-
 	public static String implodeStringArray(String separator, String[] strings) {
 		String toReturn = strings[0];
 		for (int i = 1; i < strings.length; i++) {
@@ -20,5 +15,22 @@ public class General {
 	}
 	public static String implodeStringArray(String[] s) {
 		return implodeStringArray(", ", s);
+	}
+
+	public static final class ExtraParametersNotApplicableException extends IllegalArgumentException {
+		public ExtraParametersNotApplicableException(String methodName, String[] parameters) {
+			super("The extra argument" + (parameters.length > 1 ? "s" : "") + " for " + methodName + "(..." + implodeStringArray(parameters) + ") " + (parameters.length > 1 ? "are" : "is") + "n't used there");
+		}
+	}
+
+	/**
+	 * If you've ever pressed F3 (go to declaration in Eclipse) on something robot-related that you didn't write, you know what this is for.
+	 * @author FRC 4739 Thunderbolts Robotics
+	 * @version 2016-07-14/00
+	 */
+	public static class WPILibJExemplifiesPoorProgrammingException extends Exception {
+		public WPILibJExemplifiesPoorProgrammingException(String s) {
+			super(s);
+		}
 	}
 }
